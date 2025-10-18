8e68d48991c071a2300d4eb45bb9cd0f03b16a5e
Andre Gonzales <gibstock@gmail.com>
2025-10-18 00:30:38 -0700

    add logs for subs fetch

--- app/components/SubscriptionList.tsx
+++ app/components/SubscriptionList.tsx
@@ -14,7 +14,8 @@ export default function SubscriptionList({
   subscriptions,
 }: SubscriptionListProps) {
   const [subs, setSubs] = useState(subscriptions);
-
+  console.log('subscriptions: ', subscriptions);
+  console.log('subs: ', subs);
   const handleDelete = async (id: string) => {
     if (!window.confirm('Are you sure you want to delete this subscription?')) {
       return;
